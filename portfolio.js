396072165743c1cd65ed59cb8cdddcb25adbbfad
manya vaish <manya.vsh@gmail.com>
2025-04-27 15:07:29 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,17 @@
+// let sections=document.querySelectorAll('section');
+// let links= document.querySelectorAll('.navbar-nav .nav-link ');
+// window.onscroll =()=>{
+//     sections.forEach(sec =>{
+//         let top= window.scrollY;
+//     let offset= sec.offsetTop -150; 
+// let height =sec.offsetHeight;
+// let id = sec.getAttribute('id');
+
+// if(top>=offset&& top <offset+height){
+//     links.forEach(link=>{
+//         link.classList.remove('active');
+//         document.querySelector('.navbar-nav .nav-link a[href*=' +id+']').classList.add('active');
+//     });
+// };
+//    });
+// };
